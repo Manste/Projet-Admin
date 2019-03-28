1564f1657f7383c41a0457ca547a94a3024d5d62
darrylbilongo <d.bilongo@students.ephec.be>
2019-03-28 14:20:52 +0100

    commit du mail - fichiers de config

--- mail/setup.sh
+++ mail/setup.sh
@@ -1,25 +1,12 @@
 #!/bin/bash
 
 # Set up for the mail server
+#
+set -e
 
-postconf -e myhostname=mail.wt11.ephec-ti.be
-
-mydomain=wt11.ephec-ti.be 
-
-myorigin=$mydomain
-
-"mydestination = wt11.ephec-ti.be localhost.$mydomain localhost"
-
-inet_interfaces=all 
-
-inet_protocols=ipv4 
-
-mailbox_size_limit=0 
-
-mynetworks=127.0.0.0/8
-
+set -x
 
 # Reload postfix to accept the changes
 
-service postfix restart
+/etc/init.d/postfix start
 
