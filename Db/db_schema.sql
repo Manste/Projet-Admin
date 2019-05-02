6140894598f677555a8f73540aaf8a7cc1cb87e0
darrylbilongo <d.bilongo@students.ephec.be>
2019-05-02 16:41:10 +0200

    DB qui fonctionne !!!

--- Db/db_schema.sql
+++ Db/db_schema.sql
@@ -2,19 +2,37 @@ create database WoodyToys;
 
 use WoodyToys;
 
-create table Client(
+CREATE TABLE `virtual_domains` (
+        `id`  INT NOT NULL AUTO_INCREMENT,
+        `name` VARCHAR(50) NOT NULL,
+        PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-clientId char(4) not null,
-primary_key(clientId)
+CREATE TABLE `virtual_users` (
+        `id` INT NOT NULL AUTO_INCREMENT,
+        `domain_id` INT NOT NULL,
+        `password` VARCHAR(106) NOT NULL,
+        `email` VARCHAR(120) NOT NULL,
+        PRIMARY KEY (`id`),
+        UNIQUE KEY `email` (`email`),
+        FOREIGN KEY (domain_id) REFERENCES virtual_domains(id) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-) COMMENT="Table client";
+CREATE TABLE `virtual_aliases` (
+        `id` INT NOT NULL AUTO_INCREMENT,
+        `domain_id` INT NOT NULL,
+        `source` varchar(100) NOT NULL,
+        `destination` varchar(100) NOT NULL,
+        PRIMARY KEY (`id`),
+        FOREIGN KEY (domain_id) REFERENCES virtual_domains(id) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-# Comptes utilisateurs
+-- Comptes utilisateurs
 
-create user 'admin'@'localhost' identified as 'admin_password';
+-- create user 'admin'@'localhost' identified as 'admin_password';
 
-grant all privileges on '.' to 'admin'@'localhost';
+-- grant all privileges on '.' to 'admin'@'localhost';
 
-# FLUSH
+-- FLUSH
 
 flush privileges;
