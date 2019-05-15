6645b60498f585b6d3bd1e82c5e471dd48f6a243
Manste <root@vps651907.ovh.net>
2019-05-16 01:18:42 +0200

    RAS: je ne n'ai rien fait de spécial

--- mail/config.sh
+++ mail/config.sh
@@ -1,20 +1,6 @@
-#cd /var/www/html && wget https://github.com/roundcube/roundcubemail/releases/download/1.3.6/roundcubemail-1.3.6-complete.tar.gz
-
-#tar xvf roundcubemail-1.3.6-complete.tar.gz
-
-#sudo mv roundcubemail-1.3.6 /var/www/html/roundcube
-
-#sudo chmod 775 /var/www/html/roundcube/temp/ /var/www/html/roundcube/logs/
-
-#Pour que RoundCube fonctionne sans problème, on active le module Apache Mod Rewrite
-#sudo a2enmod rewrite
-
 openssl req -new -newkey rsa:4096 -days 365 -nodes -x509 \
     -subj "/C=BE/ST=Ottignies/L=Louvain-la-Neuve/O=WoodyToys/CN=www.wt11.ephec-ti.be" \
     -keyout "/etc/ssl/private/dovecot.pem"  -out "/etc/ssl/certs/dovecot.pem"
-
-#find /etc/ssl -name dovecot.* -exec rm {} \;
-#dpkg-reconfigure dovecot-common
 
 mkdir -p /var/mail/vhosts/wt11.ephec-ti.be
 groupadd -g 5000 vmail 
@@ -25,6 +11,11 @@ chown -R vmail:dovecot /etc/dovecot
 chmod -R o-rwx /etc/dovecot 
 
 service mysql start
-#service postfix restart
 /etc/init.d/mysql start
+
+openssl genrsa -out /etc/opendkim/opendkim.key 1024
+openssl rsa -in /etc/opendkim/opendkim.key -pubout -out /etc/opendkim/opendkim.pub.key
+chmod "u=rw,o=,g=" /etc/opendkim/opendkim.key
+chown opendkim:opendkim /etc/opendkim/opendkim.key
+chmod 600 /etc/opendkim/opendkim.key
 
